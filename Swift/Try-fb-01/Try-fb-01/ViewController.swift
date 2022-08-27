2b2546b38b8c0bb185aba5d63bf6bab7b76bf6eb
Muhammad Abdul Fattah <84857518+fattah247@users.noreply.github.com>
2022-08-27 22:22:50 +0700

    Learning Firebase

---
+++ Swift/Try-fb-01/Try-fb-01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Try-fb-01
+//
+//  Created by Muhammad Abdul Fattah on 27/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Swift/Try-fb-01/Try-fb-01UITests/Try_fb_01UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Try_fb_01UITestsLaunchTests.swift
+//  Try-fb-01UITests
+//
+//  Created by Muhammad Abdul Fattah on 27/08/22.
+//
+
+import XCTest
+
+class Try_fb_01UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
