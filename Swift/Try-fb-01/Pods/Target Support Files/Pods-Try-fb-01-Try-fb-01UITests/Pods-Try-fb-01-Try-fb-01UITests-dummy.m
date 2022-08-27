1247789ff04d3ad0aaf4495d0e09f16032b2cf57
Muhammad Abdul Fattah <84857518+fattah247@users.noreply.github.com>
2022-08-27 22:48:32 +0700

    Firebase-01

---
+++ Swift/Try-fb-01/Pods/Target Support Files/Pods-Try-fb-01-Try-fb-01UITests/Pods-Try-fb-01-Try-fb-01UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Try_fb_01_Try_fb_01UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Try_fb_01_Try_fb_01UITests
+@end
---
+++ Swift/Try-fb-01/Pods/Target Support Files/Pods-Try-fb-01Tests/Pods-Try-fb-01Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Try_fb_01Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Try_fb_01Tests
+@end
