d1e72c4b6906e0ec29543d1fcc86bf7ca6c11684
Muhammad Abdul Fattah <84857518+fattah247@users.noreply.github.com>
2022-08-29 08:45:27 +0700

    Firebase-05.01

--- Swift/Firebase/HighWaters/HighWaters/AppDelegate.swift
+++ Swift/Firebase/HighWaters/HighWaters/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseCore
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    //to call Firebase and configure firebase
+    FirebaseApp.configure()
     // Override point for customization after application launch.
     return true
   }
