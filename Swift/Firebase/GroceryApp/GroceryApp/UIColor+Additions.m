24afe2b5917b13afe2ed913436bb9cae12c1aba6
Muhammad Abdul Fattah <84857518+fattah247@users.noreply.github.com>
2022-09-02 13:24:31 +0700

    Firebase02-02
    Adding storyboard - Copy

--- Swift/Firebase/GroceryApp/GroceryApp/UIColor+Additions.m
+++ Swift/Firebase/GroceryApp/GroceryApp/UIColor+Additions.m
@@ -21,6 +21,13 @@
   
   unsigned int baseValue;
   [[NSScanner scannerWithString:cleanString] scanHexInt:&baseValue];
+  
+  float red = ((baseValue >> 24) & 0xFF)/255.0f;
+  float green = ((baseValue >> 16) & 0xFF)/255.0f;
+  float blue = ((baseValue >> 8) & 0xFF)/255.0f;
+  float alpha = ((baseValue >> 0) & 0xFF)/255.0f;
+  
+  return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
 
 +(UIColor *) colorForNavigationBarBackground{
