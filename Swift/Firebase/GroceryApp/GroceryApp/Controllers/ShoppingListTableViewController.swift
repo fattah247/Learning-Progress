1205379fc53dec9d7fb4d236f5fcd36a6e6bff5b
Muhammad Abdul Fattah <84857518+fattah247@users.noreply.github.com>
2022-09-11 23:27:47 +0700

    Firebase02-06
    Able to add node to firebase

--- Swift/Firebase/GroceryApp/GroceryApp/Controllers/ShoppingListTableViewController.swift
+++ Swift/Firebase/GroceryApp/GroceryApp/Controllers/ShoppingListTableViewController.swift
@@ -21,10 +21,39 @@ class ShoppingListTableViewController: UITableViewController, AddShoppingListTab
   override func viewDidLoad(){
     super.viewDidLoad()
     
+    //Referencing to database
     self.rootRef = Database.database().reference()
-    
-    
+    //The title size
     self.navigationController?.navigationBar.prefersLargeTitles = true
+    //Populating the data in database
+    populateShoppingLists()
+    
+  }
+  
+  private func populateShoppingLists(){
+    
+    //Observing type of an event, any change in data will be observed
+    //snapshot will contain the root
+    self.rootRef.observe(.value){snapshot in
+      
+      //After getting the snapshot, it will remove all view, before add the updated one
+      self.shoppingLists.removeAll()
+      
+      let shoppingListDictionary = snapshot.value as? [String:Any] ?? [:]
+      
+      for (key, _) in shoppingListDictionary{
+        
+        if let shoppingListDictionary = shoppingListDictionary[key] as? [String:Any]{
+          
+          if let shoppingList = ShoppingList(shoppingListDictionary){
+            self.shoppingLists.append(shoppingList)
+          }
+        }
+      }
+      DispatchQueue.main.async {
+        self.tableView.reloadData()
+      }
+    }
   }
   
   
@@ -41,6 +70,12 @@ class ShoppingListTableViewController: UITableViewController, AddShoppingListTab
     self.shoppingLists.append(shoppingList)
     
     //adding to Firebase
+    
+    //Giving reference
+    let shoppingListRef = self.rootRef.child(shoppingList.title)
+    //Make a node with dictionary
+    shoppingListRef.setValue(shoppingList.toDictionary())
+    
     //To close the card after save it.
     controller.dismiss(animated: true, completion: nil)
     
