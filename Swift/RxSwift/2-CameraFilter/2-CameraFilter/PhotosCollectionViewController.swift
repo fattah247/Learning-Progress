e8333fab4e99c7cb7de98b84755873fe3a5c692f
Muhammad Abdul Fattah <84857518+fattah247@users.noreply.github.com>
2022-09-21 23:01:05 +0700

    rxswift-05
    Ui Done

---
+++ Swift/RxSwift/2-CameraFilter/2-CameraFilter/PhotosCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewController.swift
+//  2-CameraFilter
+//
+//  Created by Muhammad Abdul Fattah on 21/09/22.
+//
+
+import Foundation
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController{
+  
+}
---
+++ Swift/RxSwift/2-CameraFilter/2-CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2-CameraFilter
+//
+//  Created by Muhammad Abdul Fattah on 21/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationController?.navigationBar.prefersLargeTitles = true
+   
+  }
+
+
+}
+
