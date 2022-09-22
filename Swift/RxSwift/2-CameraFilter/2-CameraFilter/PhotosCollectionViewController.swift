1dc1638052a9cb765c53291af4207cb95e7af0e0
Muhammad Abdul Fattah <84857518+fattah247@users.noreply.github.com>
2022-09-22 20:07:31 +0700

    rxswift-10
    Applying Filter

--- Swift/RxSwift/2-CameraFilter/2-CameraFilter/PhotosCollectionViewController.swift
+++ Swift/RxSwift/2-CameraFilter/2-CameraFilter/PhotosCollectionViewController.swift
@@ -13,7 +13,9 @@ import Photos
 
 class PhotosCollectionViewController: UICollectionViewController{
   
+  //It's a Subject in RxSwift
   private let selectedPhotoSubject = PublishSubject<UIImage>()
+  //Making the subject observable, so any controller can subscribe to it
   var selectedPhoto: Observable<UIImage> {
     return selectedPhotoSubject.asObservable()
   }
@@ -27,73 +29,42 @@ class PhotosCollectionViewController: UICollectionViewController{
     populatePhotos()
   }
   
+  //MARK: -
   override func numberOfSections(in collectionView: UICollectionView) -> Int {
     return 1 // 1 becase we don't use any section
   }
-  
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return self.images.count
   }
   
-  
+  //MARK: -
   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     
-    
+    //To get the selected Asset
     let selectedAsset = self.images[indexPath.row]
+    /*
+     But it will only take the PHAsset
+     But we need to get the actual Value of it
+     */
+    
+    //Get the actual Image
     PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
       
-      guard let info = info else { return }
-      
-      let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
-      
-      if !isDegradedImage {
-        
-        if let image = image {
-          self?
-            .selectedPhotoSubject.onNext(image)
-          self?.dismiss(animated: true, completion: nil)
+        guard let info = info else { return }
+
+      //Will tell either the image is degraded or not
+        let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+
+      //Will let the orignal image out if it's not degraded.
+        if !isDegradedImage {
+          if let image = image {
+            self?
+              .selectedPhotoSubject.onNext(image)
+            self?.dismiss(animated: true, completion: nil)
+          }
         }
-        
       }
-    }
   }
-    
-//  }
-//  let selectedAsset = self.images[indexPath.row]
-//  PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self], info in
-//
-//      guard let info = info else { return }
-//
-//      let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
-//
-//      if !isDegradedImage {
-//        if let image = image {
-//          self?
-//            .selectedPhotosSubject.onNext(image)
-//          self?.dismiss(animated: true, completion: nil)
-//        }
-//      }
-//    }
-    
-//    let selectedAsset = self.images[indexPath.row]
-//    PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
-//
-//        guard let info = info else { return }
-//
-//        let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
-//
-//        if !isDegradedImage {
-//
-//            if let image = image {
-//                self?
-//                .selectedPhotoSubject.onNext(image)
-//                self?.dismiss(animated: true, completion: nil)
-//            }
-//
-//        }
-//
-//    }
-//  }
     
   //Will going to be responsible for giving UICollectionViewCell
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
