d7630f54f8db50ebddb260e31328b8b28d6b71ea
fattah247 <muhammad.abdul92@ui.ac.id>
2023-04-04 08:19:37 +0700

    Angela Yu - UI Sliders

---
+++ Swift/Courses /Angela Yu - Complete BootCamp/BMI-Calculator-iOS13-Completed-master/BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    var calculatorBrain = CalculatorBrain()
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        /*
+         UISlider format will give you decimal value up to 4 number below coma.
+         That is why this contanst is used to reformatting this decimals. So it would have only 2 numbers below coma.
+         After changing it into 2 numbers below coma, it would change the type into a String so it could be input to label.
+         */
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        /*
+         It is too, to reformat the decimal but making it have no number below coma.
+         It would also change the value into a string so you would be able to input it on a label.
+         */
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+
+        calculatorBrain.calculateBMI(height: height, weight: weight)
+        performSegue(withIdentifier: "goToResult", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
